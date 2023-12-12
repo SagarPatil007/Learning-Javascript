22bad596d659f0fd05b1dda4e1f984b4707d0bf9
sagar patil <sagarpatil98754@gmail.com>
2023-12-12 20:12:22 +0530

    lect 7 added

---
+++ Lecture 7/test.js
@@ -0,0 +1,21 @@
+let value = 3 
+let negvalue = -value
+
+console.log(negvalue);
+
+let str1 = "sagar"
+
+let str2 = "patil"
+
+let str3 = str1 + str2
+
+console.log(str3);
+
+
+console.log("1" + 2);
+
+console.log(1 + "2");
+
+console.log("1" + 2 + 2);
+
+console.log(1 + 2 + "2");
