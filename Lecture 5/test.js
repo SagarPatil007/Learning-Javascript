4a4285f84cad48143653e1a1584b6fe76510a8c3
sagar patil <sagarpatil98754@gmail.com>
2023-12-12 19:56:53 +0530

    lect 5 added

---
+++ Lecture 5/test.js
@@ -0,0 +1,25 @@
+"use strict"; //    treat all JS code as newer version
+
+// alert(3+3) // we are using node.js , not broswer
+
+console.log( 3 + 3);
+
+// Official doc for Ecma Javascript =>  https://tc39.es/ecma262/
+
+
+let name = "sagar"
+
+let age = 18
+
+let isLoggedIn = true
+
+// number => 2 to power 53
+// bigint 
+// string => ""
+// boolean => true/false
+// null => standalone
+// undefined => 
+// symbol = unique
+// object
+
+console.log(typeof(name));
