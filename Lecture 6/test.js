e33bfc1b417fd1a8076de70c34a095b7c455a4b4
sagar patil <sagarpatil98754@gmail.com>
2023-12-12 20:03:25 +0530

    lect 6 added

---
+++ Lecture 6/test.js
@@ -0,0 +1,20 @@
+let score1 = 33
+
+console.log(typeof(score1));
+
+let score2 = "33abc"
+
+console.log(typeof(score2));    
+
+let valueInNumber = Number(score2)
+
+console.log(typeof(valueInNumber));
+
+console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NAN
+// true => 1 and false => 0
+
+// 
