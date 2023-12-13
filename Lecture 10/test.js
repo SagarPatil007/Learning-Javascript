8e924d9ad9f4569d44ee7380b3b82fc63d2d1235
sagar patil <sagarpatil98754@gmail.com>
2023-12-13 12:18:07 +0530

    lect 10 added

---
+++ Lecture 10/test.js
@@ -0,0 +1,25 @@
+ // stack(primitive) and heap(non-primitive)
+
+let myYTName = "chaiAurCode"
+
+let anotherName = myYTName
+
+anotherName = "CodeAurchai"
+
+console.log(myYTName);
+
+console.log(myYTName);
+
+
+let obj1 = {
+    name : "sagar",
+    age : 22
+}
+
+let obj2 = obj1
+
+obj2.name = "patil"
+
+console.log(obj1);
+console.log(obj2);
+
