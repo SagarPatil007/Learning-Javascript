1549b94b54344bfacf0ad3e9237d7e6358b33424
sagar patil <sagarpatil98754@gmail.com>
2023-12-13 23:27:16 +0530

    lect 12 added

---
+++ Lecture 12/test.js
@@ -0,0 +1,30 @@
+// const Score = 400
+
+// console.log(Score);
+
+// const balance = new Number(100)
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+// const number = 100000000
+// console.log(number.toLocaleString('en-IN'));
+
+// console.log(Math);
+
+// console.log(Math.abs(-8));
+
+// console.log(Math.round(4.3));
+// console.log(Math.floor(4.2));
+// console.log(Math.ceil(4.2));
+// console.log(Math.sqrt(25));
+// console.log(Math.pow(2,3));
+// console.log(Math.min(2,3,8,9,1));
+
+// console.log(Math.floor(Math.random()*10)+1);
+
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max-min+1)) + min );
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 A code repository to learn javascript from chai aur code (hitesh choudhary)
+
+Repo : https://github.com/SagarPatil007/Learning-Javascript
