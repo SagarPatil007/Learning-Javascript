2fc06582a72c0777843964c5742c259e47593dca
sagar patil <sagarpatil98754@gmail.com>
2023-12-13 11:54:18 +0530

    lect 9  added

---
+++ Lecture 9/test.js
@@ -0,0 +1,25 @@
+// Primitive
+
+// 7 types : string , boolean , number , null , undefined , Symbol , BigInt
+
+
+
+// Reference (non primitives)
+
+// Array , Objects , Functions
+
+let names = ["sagar","rohit","raj"]
+
+console.log(typeof names);
+
+let obj = {
+    name : "sagar",
+    age : 22
+}
+
+console.log(obj);
+
+
+function myfunc() {
+    console.log("Hello World");
+}
