40cb041db972e6f9eec8df971b997887cab13911
sagar patil <sagarpatil98754@gmail.com>
2023-12-23 01:25:05 +0530

    lect 40 added

---
+++ Lecture 40/index.js
@@ -0,0 +1,38 @@
+const promiseOne = new Promise(function(resolve,reject){
+    setTimeout(()=>{
+        console.log('Async task is complete');
+        resolve()
+    },1000)
+})
+
+promiseOne.then(()=>{
+    console.log('promise is complete');
+})
+
+const promiseThree = new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+        resolve({userName : "sagar" , email :"sagarpatil@gmail.com"})
+    },1000)
+}).then((user)=>{
+    console.log(user);
+})
+
+const promiseFour = new Promise((reject,resolve)=>{
+    let error = false
+    setTimeout(()=>{
+        if(error){
+            resolve({userName:"sagar",password:"1234"})
+        }else{
+            reject('Error : something went wrong')
+        }
+    },1000)
+})
+
+promiseFour.then((user)=>{
+    return user.userName
+}).then((userName)=>{
+    console.log(userName);
+}).catch((e)=>{
+    console.log(e);
+})
+
